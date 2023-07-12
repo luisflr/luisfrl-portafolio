319b93e5bbdc2e8db9538ebec594d12cb72cf715
luis <lflores@vozy.co>
2023-07-11 22:12:14 -0500

    add projects view

--- src/components/Common/Links.js
+++ src/components/Common/Links.js
@@ -7,8 +7,8 @@ function Links({ linksProject, reverse }) {
   return (
     <>
       <ul className={`${reverse ? 'order-1 mr-3' : 'ml-5'} mt-1 flex space-x-5`}>
-        {Object.entries(linksProject).map(([key, value]) => (
-          <li>
+        {Object.entries(linksProject).map(([key, value], idx) => (
+          <li key={`icon-${idx}`}>
             <a
                 href={value}
                 target='_blank' rel='noreferrer'
