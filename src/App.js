5a10b0eb6ab512a6a8117d019bf57cf43c8be7df
luisflr <lfloresr@ulasalle.edu.pe>
2024-04-10 15:47:10 -0500

    [UPD] adding analytics

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './views/Main';
 import { NotFoundPage } from './views/Pages';
-// import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react';
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <Route path='*' element={ <NotFoundPage /> } />
       </Routes>
     </Suspense>
-    {/* <Analytics /> */}
+    <Analytics />
     </>
   );
 }
